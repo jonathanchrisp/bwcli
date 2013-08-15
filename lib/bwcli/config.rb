946b27238b621a56d2367b743cb5d6f5a65ca91c
Jonathan Chrisp <jonathan.chrisp@gmail.com>
2013-08-15 14:20:31 +0100

    Updated add method to set current user if none already set by default, added alias for configuration method

--- lib/bwcli/config.rb
+++ lib/bwcli/config.rb
@@ -1,32 +1,30 @@
 require 'bwcli/configuration'
 
 module BWCLI
+
+  # Config subcommand
   class Config < Thor
 
     no_tasks do
       def configuration
         @configuration ||= BWCLI::Configuration.new
       end
+      alias :conf :configuration
     end
 
     desc "user", "Returns the current user"
     def user
-      configuration.list_current_user
+      conf.list_current_user
     end
 
     desc "login", "Authenticate current user"
     def login
-      configuration.oauth
-    end
-
-    desc "token", "Returns the current user token"
-    def token
-      configuration.current_user_access_token
+      conf.oauth
     end
 
     desc "list", "List users"
     def list
-      configuration.list_users
+      conf.list_users
     end
 
     desc "add", "Add a new user"
@@ -34,27 +32,32 @@ module BWCLI
     method_option :u, :banner => 'Username', :required => true
     method_option :p, :banner => 'Password', :required => true
     def add
-      configuration.add_user options[:e], options[:u], options[:p]
-      configuration.set_current_user options[:e], options[:u] if yes? "Would you like set this user as the current user?"
+      conf.add_user options[:e], options[:u], options[:p]
+
+      # Set as current user if no current user exists
+      return conf.set_current_user options[:e], options[:u] unless conf.current_user_exists?
+
+      # If current user exists check to switch
+      conf.set_current_user options[:e], options[:u] if yes? "Would you like set this user as the current user?"
     end
 
     desc "switch", "Switch to user"
     method_option :u, :banner => 'Username', :required => true
     method_option :e, :banner => 'Environment', :required => true
     def switch
-      configuration.set_current_user options[:e], options[:u]
+      conf.set_current_user options[:e], options[:u]
     end
 
     desc "remove", "Remove a new user"
-    method_option :e, :banner => 'Environment', :required => true
     method_option :u, :banner => 'Username', :required => true
+    method_option :e, :banner => 'Environment', :required => true
     def remove
-      configuration.remove_user options[:e], options[:u]
+      conf.remove_user options[:u], options[:e]
     end
 
-    desc "reset", "Reset the configuration file"
+    desc "reset", "Reset the conf file"
     def reset
-      configuration.reset if yes? "Are you sure you want to reset your config?"
+      conf.reset if yes? "Are you sure you want to reset your config?"
     end
 
     include Thor::Actions
